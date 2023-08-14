e69936c9aba06b495b803623fecb6a191b64f1a7
SelenKockar <selen.kockar@tedu.edu.tr>
2023-08-14 17:36:38 +0300

    Card component style updated

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,9 +4,9 @@
       <img class="image" :src="_product?.image" />
       <div class="bottom-section">
         <p class="product-title">{{ _product?.title }}</p>
-        <p class="price-info">{{ _product?.price }}</p>
-        <button class="product-detail">View</button>
+        <p class="price-info">{{ _product?.price }} $</p>
       </div>
+      <button class="product-detail">View</button>
     </div>
   </div>
 </template>
@@ -20,6 +20,16 @@ export default {
     },
   },
 };
+
+const productTitleElements = document.querySelectorAll(".product-title");
+
+productTitleElements.forEach((element) => {
+  const text = element.textContent;
+  const maxLength = 50;
+  if (text.length > maxLength) {
+    element.textContent = text.slice(0, maxLength) + "...";
+  }
+});
 </script>
 <style>
 .big-box {
@@ -37,23 +47,43 @@ export default {
   border: solid rgba(167, 33, 97, 0.37);
   border-radius: 1rem;
   margin-top: 1rem;
-  align-items: center;
 }
 .product-title {
-  gap: 5px;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
+
 .image {
   width: 180px;
   height: 180px;
-  border-radius: 5px;
-}
-.product-detail {
-  margin-bottom: 10px;
+  border-radius: 15px;
   margin-left: 10px;
 }
+
 .bottom-section {
   display: flex;
   flex-direction: column;
-  max-width: fit-content;
+  margin-left: 10px;
+}
+.product-detail {
+  border-radius: 0.7rem;
+  background-color: rgba(167, 33, 97, 0.7);
+  color: white;
+  border: none;
+  padding: 8px 15px;
+  cursor: pointer;
+  margin-bottom: 10px;
+  margin-left: 10px;
+  width: auto;
+}
+.product-detail:hover {
+  text-decoration: underline;
+  transition: text-decoration 0.3s ease-in-out;
+}
+.price-info {
+  color: rgba(167, 33, 97, 0.7);
 }
 </style>
